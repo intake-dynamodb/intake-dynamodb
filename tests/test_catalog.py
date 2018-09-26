eb722e3ad9ca3a169fb8877fc4e57f122e960c49
Jacob Tomlinson <jacob.tomlinson@informaticslab.co.uk>
2018-09-26 15:33:51 +0100

    Add a test

---
+++ tests/test_catalog.py
@@ -0,0 +1,23 @@
+
+# -*- coding: utf-8 -*-
+import numpy as np
+import os
+import pytest
+
+from iris.cube import CubeList
+
+from intake import open_catalog
+from .util import dataset  # noqa
+
+from intake_dynamodb import DynamoDBSource
+
+
+@pytest.fixture
+def catalog1():
+    path = os.path.dirname(__file__)
+    return open_catalog(os.path.join(path, 'data', 'catalog.yaml'))
+
+
+def test_catalog(catalog1, dataset):
+    source = catalog1['dynamodb_source'].get()
+    assert isinstance(source, DynamoDBSource)
